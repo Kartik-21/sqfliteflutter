e9f0b680d5341cea9527cfdb976530bd2740734e
Kartik-21 <kartikjain2926@gmail.com>
2020-08-11 18:29:41 +0530

    final application

--- lib/models/Note.dart
+++ lib/models/Note.dart
@@ -1,58 +1,67 @@
 class Note {
   int _id;
   String _title;
-  String _desc;
+  String _description;
   String _date;
-  int _pri;
+  int _priority;
 
-  Note(this._title, this._date, this._pri, [this._desc]);
+  Note(this._title, this._date, this._priority, [this._description]);
 
-  Note.withId(this._id, this._title, this._date, this._pri, [this._desc]);
-
-  int get pri => _pri;
-
-  String get date => _date;
-
-  String get desc => _desc;
-
-  String get title => _title;
+  Note.withId(this._id, this._title, this._date, this._priority,
+      [this._description]);
 
   int get id => _id;
 
-  set pri(int value) {
-    _pri = value;
+  String get title => _title;
+
+  String get description => _description;
+
+  int get priority => _priority;
+
+  String get date => _date;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
   }
 
-  set date(String value) {
-    _date = value;
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      this._description = newDescription;
+    }
   }
 
-  set desc(String value) {
-    _desc = value;
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
   }
 
-  set title(String value) {
-    _title = value;
+  set date(String newDate) {
+    this._date = newDate;
   }
 
+  // Convert a Note object into a Map object
   Map<String, dynamic> toMap() {
     var map = Map<String, dynamic>();
-
-    if (_id != null) {
+    if (id != null) {
       map['id'] = _id;
     }
     map['title'] = _title;
-    map['desc'] = _desc;
+    map['description'] = _description;
+    map['priority'] = _priority;
     map['date'] = _date;
-    map['pri'] = _pri;
+
     return map;
   }
 
+  // Extract a Note object from a Map object
   Note.fromMapObject(Map<String, dynamic> map) {
     this._id = map['id'];
     this._title = map['title'];
-    this._desc = map['desc'];
-    this._pri = map['pri'];
+    this._description = map['description'];
+    this._priority = map['priority'];
     this._date = map['date'];
   }
 }
--- lib/screens/NoteList.dart
+++ lib/screens/NoteList.dart
@@ -1,73 +1,148 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqfliteflutter/models/Note.dart';
 import 'package:sqfliteflutter/screens/NoteDetails.dart';
+import 'package:sqfliteflutter/utils/DatabaseHelper.dart';
 
 class NoteList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return NoteListState();
   }
 }
 
 class NoteListState extends State<NoteList> {
-  int count = 1;
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Note> noteList;
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    if (noteList == null) {
+      noteList = List<Note>();
+      updateListView();
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Note"),
+        title: Text('Notes'),
       ),
-      body: getListView(),
+      body: getNoteListView(),
       floatingActionButton: FloatingActionButton(
-        tooltip: "Add Items",
         onPressed: () {
-          debugPrint("floating action button");
-          navigateToNoteDetails("Add Note");
+          debugPrint('FAB clicked');
+          navigateToDetail(Note('', '', 2), 'Add Note');
         },
+        tooltip: 'Add Note',
         child: Icon(Icons.add),
       ),
     );
   }
 
-  ListView getListView() {
-    TextStyle textStyle = Theme.of(context).textTheme.headline1;
+  ListView getNoteListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
     return ListView.builder(
-        itemCount: count,
-        itemBuilder: (BuildContext context, int position) {
-          return Card(
-//            color: Colors.lightBlueAccent,
-            elevation: 3.0,
-            margin: EdgeInsets.all(5.0),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.yellow,
-                child: Icon(Icons.arrow_right),
-              ),
-              title: Text(
-                "Title",
-                style: textStyle,
-              ),
-              subtitle: Text(
-                "Subtitle",
-              ),
-              trailing: Icon(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor:
+                  getPriorityColor(this.noteList[position].priority),
+              child: getPriorityIcon(this.noteList[position].priority),
+            ),
+            title: Text(
+              this.noteList[position].title,
+              style: titleStyle,
+            ),
+            subtitle: Text(this.noteList[position].date),
+            trailing: GestureDetector(
+              child: Icon(
                 Icons.delete,
                 color: Colors.grey,
               ),
               onTap: () {
-                debugPrint("card");
-                navigateToNoteDetails("Edit Note");
+                _delete(context, noteList[position]);
               },
             ),
-          );
-        });
+            onTap: () {
+              debugPrint("ListTile Tapped");
+              navigateToDetail(this.noteList[position], 'Edit Note');
+            },
+          ),
+        );
+      },
+    );
   }
 
-  void navigateToNoteDetails(String title) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return NoteDetails(title);
+  // Returns the priority color
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  // Returns the priority icon
+  Icon getPriorityIcon(int priority) {
+    switch (priority) {
+      case 1:
+        return Icon(Icons.play_arrow);
+        break;
+      case 2:
+        return Icon(Icons.keyboard_arrow_right);
+        break;
+
+      default:
+        return Icon(Icons.keyboard_arrow_right);
+    }
+  }
+
+  void _delete(BuildContext context, Note note) async {
+    int result = await databaseHelper.deleteNote(note.id);
+    if (result != 0) {
+      _showSnackBar(context, 'Note Deleted Successfully');
+      updateListView();
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(content: Text(message));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  void navigateToDetail(Note note, String title) async {
+    bool result =
+        await Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return NoteDetail(note, title);
     }));
+
+    if (result == true) {
+      updateListView();
+    }
+  }
+
+  void updateListView() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Note>> noteListFuture = databaseHelper.getNoteList();
+      noteListFuture.then((noteList) {
+        setState(() {
+          this.noteList = noteList;
+          this.count = noteList.length;
+        });
+      });
+    });
   }
 }
